0064f7206fd8a5f56e448c951dd0ab45483b6a5b
Firzah Ghania Fairuza <ghaniafairuzafirzah@student.upi.edu>
2025-12-09 11:47:27 +0700

    Add user authentication data to auth.py

---
+++ UAS_CLI_PELANGGAN-MITRA/auth.py
@@ -0,0 +1,5 @@
+USERS = {
+    "alice": {"password": "alice123", "role": "pelanggan", "nama": "Alice Pelanggan"},
+    "budi":  {"password": "budi123",  "role": "mitra",      "nama": "Budi Mitra"},
+    "tina":  {"password": "tina123",  "role": "pelanggan", "nama": "Tina Pelanggan"},
+}
