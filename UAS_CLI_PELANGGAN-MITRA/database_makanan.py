25376800e8dac91c243dbebe2fcf1ba6d31b54d1
azizhrdyn <azizhardiyan@gmail.com>
2025-12-10 22:10:20 +0700

    Update database_makanan.py

--- UAS_CLI_PELANGGAN-MITRA/database_makanan.py
+++ UAS_CLI_PELANGGAN-MITRA/database_makanan.py
@@ -4,3 +4,9 @@ from pathlib import Path
 CSV_PATH = Path("makanan.csv")
 
 DEFAULT_COLUMNS = ["nama", "restoran", "kalori", "harga"]
+
+def ensure_csv_exists():
+    if not CSV_PATH.exists():
+        # buat file contoh kosong
+        df = pd.DataFrame(columns=DEFAULT_COLUMNS)
+        df.to_csv(CSV_PATH, index=False)
